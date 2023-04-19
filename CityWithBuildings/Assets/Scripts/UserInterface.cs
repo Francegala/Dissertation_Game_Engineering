36c795e5c7b2ef863aff368fef9eaddea05c93ff
Francegala <francescogalassimobile@gmail.com>
2023-04-19 21:31:42 +0100

    procedural generated city with procedural generated building - waorking walkable app

---
+++ CityWithBuildings/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour
+{
+
+ public void Info()
+ {
+  
+ }
+
+ public void closeApp()
+ {
+  Application.Quit();
+
+ }
+
+
+ void Update()
+ {
+  if (Input.GetKey("escape"))
+  {
+   Application.Quit();
+  }
+  
+  // moving camera
+  if (Input.GetKey("right"))GameObject.Find("Main Camera").transform.Translate(Vector3.right* Time.deltaTime);
+
+  if (Input.GetKey("left")) GameObject.Find("Main Camera").transform.Translate(Vector3.left* Time.deltaTime);
+
+
+    if (Input.GetKey("up"))   GameObject.Find("Main Camera").transform.Translate(Vector3.forward* Time.deltaTime);
+
+     if (Input.GetKey("down"))   GameObject.Find("Main Camera").transform.Translate(-Vector3.forward* Time.deltaTime);
+
+ }
+    
+}
---
+++ CityWithBuildings/Assets/Scripts/spawnHouse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class spawnHouse : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myBuilding;
+    private List<GameObject> buildingsList  = new List<GameObject>();
+
+    public void Create(int lungoVar, int largoVar, int floorsVar)
+    {
+        if (buildingsList.Count > 0)
+        {
+            foreach (var buildingCreated in buildingsList) {
+                Destroy(buildingCreated);
+            }
+        }
+        var building =Instantiate(myBuilding);
+            building.name = "Building";
+            buildingsList.Add(building);
+            building.GetComponent<SpawnBuilding>().createBuildings(0,0, lungoVar,  largoVar,  floorsVar);
+            building.transform.localScale /= 6;
+        
+    }
+
+    public void DestroyBase()
+    {
+        if (buildingsList.Count > 0)
+        {
+            foreach (var buildingCreated in buildingsList) {
+                Destroy(buildingCreated);
+            }
+        }   
+    }
+}
